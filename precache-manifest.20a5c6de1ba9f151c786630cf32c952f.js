80f641103fdbab1d2171cc331e6c9af69cef152c
KiyoungLee73 <viracoch@naver.com>
2020-03-06 09:09:03 +0900

    Updates

---
+++ precache-manifest.20a5c6de1ba9f151c786630cf32c952f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b49df07dbc23906997fdf91952e3073",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7e722c3486eac3c0f38e",
+    "url": "/movie_app_2020/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "e20af58440bf71d6eac9",
+    "url": "/movie_app_2020/static/js/2.0320cdf4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.0320cdf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e722c3486eac3c0f38e",
+    "url": "/movie_app_2020/static/js/main.491a6b5b.chunk.js"
+  },
+  {
+    "revision": "d12489d94ca1021bfcf8",
+    "url": "/movie_app_2020/static/js/runtime-main.afb475c1.js"
+  }
+]);
